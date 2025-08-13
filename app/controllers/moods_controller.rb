4676c1c9f07809fc39e8cd4d4c827c2f4b5ec495
r070624tue <h290404tue@gmail.com>
2025-08-13 11:25:40 +0900

    コントローラーの編集

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -2,4 +2,15 @@ class MoodsController < ApplicationController
   def new
     @mood = Mood.new
   end
+
+  def create
+    Mood.create(mood_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def mood_params
+    params.require(:mood).permit(:score, :date_on).merge(user_id: current_user.id)
+  end
 end
