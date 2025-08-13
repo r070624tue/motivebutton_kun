c49abc08d8d40fe5c2f3689ed369f71fd5c7d956
r070624tue <h290404tue@gmail.com>
2025-08-13 17:47:58 +0900

    コントローラーの設定（create）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,22 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.date_on = Date.current
+    @task.status = 0
+
+    if @task.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :date_on, :status).merge(user_id: current_user.id)
+  end
 end
