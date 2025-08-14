d5a8574521da860f4f436971bb8fa77af2da4ef8
r070624tue <h290404tue@gmail.com>
2025-08-14 11:24:27 +0900

    詳細ページ遷移用コントローラーの設定

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,8 @@
 class CalendarController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @date  = Date.parse(params[:date])
+    @tasks = current_user.tasks.where(date_on: @date).order(:created_at)
   end
 end
