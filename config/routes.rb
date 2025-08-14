016d5aa3f5912b5104e93c875abd47b626e4f90e
r070624tue <h290404tue@gmail.com>
2025-08-14 14:52:33 +0900

    ルーティングの設定（update）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tasks#index"
-  resources :tasks, param: :date, only: [:index, :new, :create, :show]
+  resources :tasks, param: :date, only: [:index, :new, :create, :show, :update]
   resources :moods, only: [:new, :create]
 end
