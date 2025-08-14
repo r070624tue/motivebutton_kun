c696661012e0f18e74cfa49401c8b0d5f05a1ea8
r070624tue <h290404tue@gmail.com>
2025-08-14 11:19:47 +0900

    詳細ページリンク用のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root "tasks#index"
   resources :tasks, only: [:index, :new, :create, :show]
   resources :moods, only: [:new, :create]
+  get 'calendar/:date', to: 'calendar#show', as: :calendar_day
 end
