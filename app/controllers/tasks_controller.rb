7d1f2f2b8d440ea16eaddecafe23b53f64db63d1
r070624tue <h290404tue@gmail.com>
2025-08-14 14:52:51 +0900

    コントローラーの設定（update）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,6 +29,15 @@ class TasksController < ApplicationController
     @tasks = current_user.tasks.where(date_on: @date).order(:created_at)
   end
 
+  def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_back fallback_location: root_path
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def tasks_params
