4326ede00439086d5277ebec92efcf530ed169bf
r070624tue <h290404tue@gmail.com>
2025-08-14 17:12:57 +0900

    コントローラーの設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,11 @@ class TasksController < ApplicationController
     @tasks = current_user.tasks.where(date_on: @date).order(:created_at)
   end
 
+  def edit
+    @date  = Date.parse(params[:date])
+    @tasks = current_user.tasks.where(date_on: @date).order(:created_at)
+  end
+
   def update
     @task = current_user.tasks.find(params[:id])
     @date = @task.date_on
