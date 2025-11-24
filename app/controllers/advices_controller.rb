5e780af5ea33dca6c46f011cc775e25bea9f8675
r070624tue <h290404tue@gmail.com>
2025-11-24 17:56:42 +0900

    プロンプト改善

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -16,7 +16,7 @@ class AdvicesController < ApplicationController
     mood_name_text = @mood.mood_name
     mood_score_text = "スコアが#{@mood.score}点"
     task_list = @tasks.map(&:content).join('、')
-    prompt = "今日の気分は「#{mood_name_text}」（#{mood_score_text}）で、やるべきタスクは「#{task_list}」です。この状況を踏まえて、ポジティブなアドバイスを100文字程度でお願いします。"
+    prompt = "あなたは世界最高のアドバイザーです。今日の気分とタスクを比較してタスクの量や質が妥当か、妥当ではない場合はどのように改善するのが良いかアドバイスを200文字以内でください。今日の気分は「#{mood_name_text}」（#{mood_score_text}）で、やるべきタスクは「#{task_list}」です。"
 
     begin
       advice_text = openai_api_call(prompt)
